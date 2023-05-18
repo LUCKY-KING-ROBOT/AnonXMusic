4eb959ac9fe7051a688bacf2a806204404acc323
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-05-18 18:45:14 +0530

    Update start.py
    Signed-off-by: LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -5,7 +5,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import config
 
 
-def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+def mstart_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
