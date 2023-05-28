cb0f188329cd9fa775029cda5964ddd9136e0f86
LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>
2023-05-28 12:36:41 +0530

    Update help.py
    Signed-off-by: LUCKY-KING-ROBOT <127356753+LUCKY-KING-ROBOT@users.noreply.github.com>

--- AnonX/plugins/help.py
+++ AnonX/plugins/help.py
@@ -59,7 +59,7 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
+        await update.reply_sticker("CAACAgEAAx0CcqM2ZwABATqoZHL9JrEEfrLeNpEQQMhY-pjM2nIAAk0DAALZN6lFv6l9QgkDaXYvBA")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard)
